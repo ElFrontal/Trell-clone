f2bd49e6d7d3cf2a3bfa36169f9d871ab8ac09e4
Oleg <oleg.a.kuzmin@mail.ru>
2019-11-14 23:22:51 +0800

    21212

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ Application.load()
 document
     .querySelector('[data-action-addColumn]')
     .addEventListener('click', function (evt) {
-        const columnElement = Column.create()
-        document.querySelector('.columns').append(columnElement)
+        const column = new Column
+        document.querySelector('.columns').append(column.element)
 
         Application.save()
     })
